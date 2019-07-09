b85ce08f2657c9f183faf45afcb458692cab3211
ZhongZong <1291988914@qq.com>
2019-07-09 15:15:47 +0800

    该项目将用于实现微信公众号开发

--- src/main/java/com/zzk/ssmdemo/controller/TestController.java
+++ src/main/java/com/zzk/ssmdemo/controller/TestController.java
@@ -1,24 +1,28 @@
 package com.zzk.ssmdemo.controller;
 
 import com.zzk.ssmdemo.entity.User;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@EnableAutoConfiguration
-@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/getusers")
+    @ResponseBody
     public List<User> getUsers() {
         List<User> list = new ArrayList<>();
         list.add(new User());
 
         return list;
+    }
+
+    @GetMapping("/test")
+    public ModelAndView test(){
+        return new ModelAndView("test");
     }
 }
